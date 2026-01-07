e60be18b1d4b30a78daae83eb7fae16c375016d3
Székely Ádám <szekelyadam@edu.bme.hu>
2026-01-08 00:02:15 +0100

    refactor: jobs misfire do nothing

--- backend/Soundfeed.Bll/Extensions/ServiceCollectionExtensions.cs
+++ backend/Soundfeed.Bll/Extensions/ServiceCollectionExtensions.cs
@@ -25,26 +25,25 @@ public static class ServiceCollectionExtensions
 
         services.AddQuartz(q =>
         {
-            // sync job
+            // Release sync job
             var releaseJobKey = new JobKey("ReleaseSyncJob");
             q.AddJob<ReleaseSyncJob>(opts => opts.WithIdentity(releaseJobKey));
-
             var cronSchedule = configuration["Jobs:ReleaseSyncJob"] ?? "0 0/1 * 1/1 * ? *";
-
             q.AddTrigger(opts => opts
                 .ForJob(releaseJobKey)
                 .WithIdentity("ReleaseSyncJob-Trigger")
-                .WithCronSchedule(cronSchedule));
+                .WithCronSchedule(cronSchedule, x => x
+                    .WithMisfireHandlingInstructionDoNothing()));
 
-            // user deleteion job
+            // Inactive user job
             var userJobKey = new JobKey("InactiveUserJob");
             q.AddJob<InactiveUserJob>(opts => opts.WithIdentity(userJobKey));
-
             var inactiveUserCron = configuration["Jobs:InactiveUserJob"] ?? "0 0 0 * * ?";
             q.AddTrigger(opts => opts
                 .ForJob(userJobKey)
                 .WithIdentity("InactiveUserJob-Trigger")
-                .WithCronSchedule(inactiveUserCron));
+                .WithCronSchedule(inactiveUserCron, x => x
+                    .WithMisfireHandlingInstructionDoNothing()));
         });
 
 
